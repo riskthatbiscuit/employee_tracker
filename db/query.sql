0fda2b504823e858c6a326e33e478181bcc86f39
Casey Mcgrath <caseymcgrath1000@gmail.com>
2023-07-30 16:51:16 -0400

    Added manager left join to master table

--- db/query.sql
+++ db/query.sql
@@ -17,6 +17,19 @@ JOIN department ON roles.department_id = department.id;
 
 -- WHEN I choose to view all employees
 -- THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+SELECT 
+  employee.id AS employee_id,
+  employee.first_name,
+  employee.last_name,
+  roles.title AS job_title,
+  department.name AS department,
+  roles.salary AS salary,
+  CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+FROM employee
+JOIN roles ON employee.role_id = roles.id
+JOIN department ON roles.department_id = department.id
+LEFT JOIN employee AS manager ON employee.manager_id = manager.id;
+
 -- WHEN I choose to add a department
 -- THEN I am prompted to enter the name of the department and that department is added to the database
 -- WHEN I choose to add a role
