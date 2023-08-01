de3a8b18707a563c953647be68a2e3fbf9bed1d6
Casey Mcgrath <caseymcgrath1000@gmail.com>
2023-07-31 21:23:24 -0400

    Added +department and +role options

--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE department (
 );
 
 CREATE TABLE roles (
-  id INT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL, 
   department_id INT,
@@ -19,7 +19,7 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE employee (
-  id INT PRIMARY KEY, 
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT, 
--- questions.js
+++ questions.js
@@ -12,6 +12,55 @@ const question1 = [
      }
     ]
 
+const question2 = [
+    {
+        type: 'input',
+        name: 'departmentName',
+        message: 'What department would you like to add?',
+    }
+]
+
+const question3 = [
+  {
+    type: "input",
+    name: "role",
+    message: "What is the role?",
+  },
+  {
+    type: "input",
+    name: "salary",
+    message: "What is the salary of the role?",
+  },
+  {
+    type: "input",
+    name: "department",
+    message: "What is the id of the department to which the role belongs?",
+  }
+];
+
+const question4 = [
+  {
+    type: "input",
+    name: "firstName",
+    message: "What is the first name of the employee?",
+  },
+  {
+    type: "input",
+    name: "lastName",
+    message: "What is the last name of the employee?",
+  },
+  {
+    type: "input",
+    name: "role",
+    message: "What is the role they will do?",
+  },
+  {
+    type: "input",
+    name: "managerName",
+    message: "Who is the person's manager?",
+  }
+];
+
 // Create a function to initialize app
 function init(db) {
 
@@ -107,4 +156,38 @@ function viewAllEmployees(db, callback) {
     JOIN department ON roles.department_id = department.id
     LEFT JOIN employee AS manager ON employee.manager_id = manager.id`;
   executeQuery(db, sqlQuery, callback);
+}
+
+function addDepartment(db, runQueryLoop) {
+  inquirer.prompt(question2).then((answers) => {
+    const departmentName = answers.departmentName;
+    const sqlQuery = "INSERT INTO department (name) VALUES (?)";
+
+    db.query(sqlQuery, [departmentName], (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`Could not make change`);
+        runQueryLoop();
+      }
+    });
+  });
+}
+
+function addRole(db, runQueryLoop) {
+  inquirer.prompt(question3).then((answers) => {
+    const role = answers.role;
+    const salary = answers.salary;
+    const department_id = answers.department_id;
+    const sqlQuery = "INSERT INTO roles (title, salary, department_id) VALUES (?,?,?);";
+
+    db.query(sqlQuery, [role, salary, department_id], (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`Could not make change`);
+        runQueryLoop();
+      }
+    });
+  });
 }
